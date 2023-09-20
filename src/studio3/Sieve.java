441af78c779d70f21449d3c5140c887e6a6c3ac6
ckind <ckind@TABLET-RO0JC1OM.dhcp.wustl.edu>
2023-09-20 14:19:47 -0500

    sieve work

---
+++ src/studio3/Sieve.java
@@ -0,0 +1,19 @@
+package studio3;
+import java.util.Scanner;
+
+public class Sieve {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner in = new Scanner(System.in);
+		System.out.println("what is the value of n?");
+		int n = in.nextInt();
+		
+		boolean[] array;
+		array = new boolean[n];
+		
+		for (double count =1;count<=n;count++);
+	}
+
+}
